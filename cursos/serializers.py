11796e4c286ad29f03f9fb0c9172888871dd52d3
Yago Ferreira <Yago.rox@hotmail.com>
2020-08-31 14:17:20 -0300

    First Commit

---
+++ cursos/serializers.py
@@ -0,0 +1,49 @@
+from rest_framework import serializers
+
+from .models import Curso, Avaliacao
+
+
+class AvaliacaoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+        model = Avaliacao
+        fields = (
+            'id',
+            'curso',
+            'nome',
+            'email',
+            'comentario',
+            'avaliacao',
+            'criacao',
+            'ativo'
+        )
+
+
+class CursoSerializer(serializers.ModelSerializer):
+    # Nested Relationship
+    # avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
+
+    # HyperLinked Related Field
+    """
+    avaliacoes = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='avaliacao-detail'
+    )
+    """
+    # Primary Key Related Field
+    avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Curso
+        fields = (
+            'id',
+            'titulo',
+            'url',
+            'criacao',
+            'ativo',
+            'avaliacoes'
+        )
